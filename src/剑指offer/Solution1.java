da65565441a90937a98ea4f7c0ac03314fd6ce68
LeonAng <lvang_leon@qq.com>
2017-08-24 18:04:18 +0800

    2017/8/24

---
+++ src/剑指offer/Solution1.java
@@ -0,0 +1,41 @@
+package 剑指offer;
+/**
+ * @author 12083
+ *在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ *请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+public class Solution1
+{
+	public static boolean Find(int target, int [][] array)
+	{
+		int x=0;
+		int y=0;
+		while(x<=array.length-1)
+		{
+			while(y<=array[x].length-1)
+			{
+				if(array[x][y]==target)
+				{
+					return true;
+				}
+				else
+				{
+					y++;
+				}
+			}
+			x++;
+			y=0;
+		}
+		return false;
+	}
+	public static void main(String[] args)
+	{
+		int [][] array = new int [][]{{0,1,2,3,4,5,7,9,11,13},
+			                          {4,6,8,12,15,16,17,18,20,21}};
+	    int target1 = 1;
+	    int target2 = 100;
+	    
+	    System.out.println("第一个数："+Find(target1,array));
+	    System.out.println("第二个数："+Find(target2,array));
+	}
+}
---
+++ src/剑指offer/Solution3.java
@@ -0,0 +1,47 @@
+package 剑指offer;
+/**
+ * @author 12083
+ *输入一个链表，从尾到头打印链表每个节点的值。
+ */
+import java.util.ArrayList;
+class ListNode 
+{
+	int val;
+    ListNode next = null;
+
+    ListNode(int val) 
+    {
+        this.val = val;
+    }
+}
+public class Solution3
+{
+	static ArrayList<Integer> result =new ArrayList<Integer>();
+	public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) 
+	{
+		if(listNode!=null)
+		{
+            printListFromTailToHead(listNode.next);
+            result.add(listNode.val);
+        }
+		return result;
+	}
+	
+	public static void main(String[] args)
+	{
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = new ListNode(2);
+		ListNode node3 = new ListNode(3);
+		
+		node1.next=node2;
+		node2.next=node3;
+		
+		ArrayList<Integer> result=printListFromTailToHead(node1);
+		
+		int length=result.size();
+		for(int i=0;i<length;i++)
+		{
+			System.out.println(result.get(i));
+		}
+	}
+}
