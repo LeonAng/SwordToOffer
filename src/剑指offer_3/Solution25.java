f6757b693ab264d3dfd5577fa9f4eefebd04c5b0
LeonAng <lvang_leon@qq.com>
2017-08-30 18:15:54 +0800

    2017/8/30
    26

--- src/剑指offer_3/Solution25.java
+++ src/剑指offer_3/Solution25.java
@@ -1,4 +1,5 @@
 package 剑指offer_3;
+
 /** 
 * @author Leon 
 * @version 创建时间：2017年8月29日 下午2:51:20 
@@ -10,7 +11,31 @@ public class Solution25
 {
 	public static RandomListNode Clone(RandomListNode pHead)
 	{
-		return null;
+		if (pHead == null)
+			return null;
+
+		RandomListNode result = new RandomListNode(pHead.label);
+		RandomListNode node1 = result;
+		RandomListNode node2 = result;
+		RandomListNode pHead1 = pHead;
+		RandomListNode pHead2 = pHead;
+
+		while (pHead1.next != null)
+		{
+			node1.next = new RandomListNode(pHead1.next.label);
+			node1 = node1.next;
+			pHead1 = pHead1.next;
+		}
+		while (pHead2 != null)
+		{
+			if (pHead2.random != null)
+			{
+				node2.random = new RandomListNode(pHead2.random.label);
+			}
+			node2 = node2.next;
+			pHead2 = pHead2.next;
+		}
+		return result;
 	}
 
 	public static void main(String[] args)
@@ -20,6 +45,34 @@ public class Solution25
 		RandomListNode node2 = new RandomListNode(2);
 		RandomListNode node3 = new RandomListNode(3);
 		RandomListNode node4 = new RandomListNode(4);
+		pHead.next = node1;
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+
+		pHead.random = node4;
+		node2.random = node1;
+		node3.random = node2;
+		node4.random = node3;
+
+		RandomListNode copyList1 = Clone(pHead);
+		while (copyList1 != null)
+		{
+			System.out.print(copyList1.label + " ");
+			copyList1 = copyList1.next;
+		}
+
+		System.out.println();
+		RandomListNode copyList2 = Clone(pHead);
+
+		while (copyList2 != null)
+		{
+			if (copyList2.random != null)
+			{
+				System.out.print(copyList2.random.label + " ");
+			}
+			copyList2 = copyList2.next;
+		}
 	}
 }
 
