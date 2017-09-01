e6c03f69f2f54fbaa81ca063dc5a946f0ba27511
LeonAng <lvang_leon@qq.com>
2017-09-01 17:15:37 +0800

    Revert "2017/9/1"
    This reverts commit f59280d15d2eabfbeb4cbdb282618b851f287b46.

--- src/剑指offer_4/Solution28.java
+++ src/剑指offer_4/Solution28.java
@@ -1,51 +1,83 @@
 package 剑指offer_4;
 
-import java.util.Arrays;
-
 /** 
 * @author Leon 
-* @version 创建时间：2017年9月1日 上午11:01:41 
+* @version 创建时间：2017年8月31日 上午11:45:55 
 * 类说明 :
-* 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
-* 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+* 	题目描述
+		输入一个字符串,按字典序打印出该字符串中字符的所有排列。
+		例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+	输入描述:
+		输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
 */
+import java.util.*;
+
 public class Solution28
 {
-	public static int MoreThanHalfNum_Solution(int[] array)
+	public static ArrayList<String> Permutation(String str)
 	{
-		if(array.length==0)
-			return 0;
-		if(array.length==1)
-			return array[0];
-		
-		int limit = array.length/2;
-		Arrays.sort(array);
-		//System.out.println(Arrays.toString(array));
-		int count=1;
-		for(int i=1;i<array.length;i++)
+		ArrayList<String> result = new ArrayList<String>();
+
+		if (str == null || str.length() == 0)
+			return result;
+
+		char[] letters = str.toCharArray();
+		/* 冒泡排序 */
+		for (int i = 0; i < letters.length; i++)
 		{
-			if(array[i]==array[i-1])
+			for (int j = i + 1; j < letters.length; j++)
 			{
-				count++;
-			}
-			else
-			{
-				if(count>limit)
+				if (letters[i] > letters[j])
 				{
-					return array[i-1];	
-				}
-				else
-				{
-					count=1;
+					char tmp = letters[i];
+					letters[i] = letters[j];
+					letters[j] = tmp;
 				}
 			}
 		}
-		return 0;
+
+		TreeSet<String> temp = new TreeSet<>();
+		sort(letters, 0, temp);
+		result.addAll(temp);
+		return result;
+	}
+
+	private static void sort(char[] chars, int begin, TreeSet<String> result)
+	{
+		if (chars == null || chars.length == 0 || begin < 0 || begin > chars.length - 1)
+		{
+			return;
+		}
+
+		if (begin == chars.length - 1)
+		{
+			result.add(String.valueOf(chars));
+		} else
+		{
+			for (int i = begin; i <= chars.length - 1; i++)
+			{
+				swap(chars, begin, i);
+				sort(chars, begin + 1, result);
+				swap(chars, begin, i);
+			}
+		}
+	}
+
+	private static void swap(char[] x, int a, int b)
+	{
+		char t = x[a];
+		x[a] = x[b];
+		x[b] = t;
 	}
 
 	public static void main(String[] args)
 	{
-		int[] array =new int[] {};
-		System.out.println(MoreThanHalfNum_Solution(array));
+		String str = "baC";
+
+		ArrayList<String> list = Permutation(str);
+		for (int i = 0; i < list.size(); i++)
+		{
+			System.out.println(list.get(i));
+		}
 	}
 }
--- src/剑指offer_4/Solution30.java
+++ src/剑指offer_4/Solution30.java
@@ -1,63 +1,53 @@
 package 剑指offer_4;
 
+import java.util.*;
+
 /** 
 * @author Leon 
-* @version 创建时间：2017年9月1日 下午3:02:56 
+* @version 创建时间：2017年9月1日 上午11:46:22 
 * 类说明 :
-* HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。
-* 今天测试组开完会后,他又发话了:
-* 在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
-* 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？
-* 例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。
-* 你会不会被他忽悠住？(子向量的长度至少是1)
+* 输入n个整数，找出其中最小的K个数。
+* 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
 */
 public class Solution30
 {
-	public static int FindGreatestSumOfSubArray(int[] array)
+	public static ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k)
 	{
-		if (array.length == 1)
-			return array[0];
-
-		int result = Find(array, 0);
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		
+		Arrays.sort(input);
+		// System.out.println(Arrays.toString(input));
+		
+		/*去重*/
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < input.length; i++)
+		{
+			set.add(input[i]);
+		}
+		
+		/*放回容器*/
+		Iterator<Integer> it = set.iterator();
+		int flag=0;
+		if(k>set.size())
+		{
+			return result;
+		}
+		while (it.hasNext()&&flag<k)
+		{
+			int num = it.next();
+			result.add(num);
+			flag++;
+		}		
 		return result;
-	}
-
-	private static int Find(int[] array, int start)
-	{
-		if (start >= array.length - 1)
-		{
-			return array[array.length - 1];
-		}
-
-		int count1 = Integer.MIN_VALUE;
-		int count2 = 0;
-		for (int i = start; i < array.length; i++)
-		{
-			for (int j = start; j <= i; j++)
-			{
-				count2 += array[j];
-			}
-			if (count2 >= count1)
-			{
-				count1 = count2;
-				count2 = 0;
-			}
-			count2 = 0;
-		}
-
-		if (count1 <= Find(array, start + 1))
-		{
-			count1 = Find(array, start + 1);
-		}
-
-		//System.out.println(count1);
-		return count1;
 	}
 
 	public static void main(String[] args)
 	{
-		int[] array = new int[] { 6, -3, -2, 7, -15, 1, 2, 2 };
-		System.out.println("result: " + FindGreatestSumOfSubArray(array));
+		int[] input = new int[] { 4, 5, 1, 6, 2, 7, 1, 1 };
+		ArrayList<Integer> list = GetLeastNumbers_Solution(input, 4);
+		for (int i = 0; i < list.size(); i++)
+		{
+			System.out.print(list.get(i)+" ");
+		}
 	}
-
 }
