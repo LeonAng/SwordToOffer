f3d7c964a66aaaf135d2ae46fdcc8eaadb307735
LeonAng <lvang_leon@qq.com>
2017-09-01 17:25:15 +0800

    2017/9/1-2
    今天周五，开心o(*￣▽￣*)ブ

---
+++ src/剑指offer_5/Solution32.java
@@ -0,0 +1,24 @@
+package 剑指offer_5;
+
+/** 
+* @author Leon 
+* @version 创建时间：2017年9月1日 下午5:22:47 
+* 类说明 :
+* 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+* 例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+*/
+import java.util.ArrayList;
+
+public class Solution32
+{
+	public String PrintMinNumber(int[] numbers)
+	{
+		return null;
+	}
+
+	public static void main(String[] args)
+	{
+
+	}
+
+}
