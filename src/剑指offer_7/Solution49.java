2dc166a804058907fde376041ce4011c7361d4e3
Leon <lvang_leon@qq.com>
2017-11-04 17:13:57 +0800

    2017/11/4
    SSGvsSKT  李哥加油！

---
+++ src/剑指offer_7/Solution49.java
@@ -0,0 +1,33 @@
+package 剑指offer_7;
+
+/** 
+* @author Leon
+* @date 创建时间：2017年11月4日 下午4:14:08
+* @version 1.0
+* 类说明 :
+* 题目描述
+	将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0
+     输入描述:
+	输入一个字符串,包括数字字母符号,可以为空
+     输出描述:
+	如果是合法的数值表达则返回该数字，否则返回0
+     示例1
+	输入
+	+2147483647
+    	1a33
+	输出
+	2147483647
+    	0
+*/
+public class Solution49
+{
+	public static int StrToInt(String str)
+	{
+		return 0;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(StrToInt("+2147483647"));
+	}
+}
